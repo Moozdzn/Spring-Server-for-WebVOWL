0a555ea72c1f56e34cdd7f736fbd2926ccc0ab7b
Diogo Santos <moozdzn@gmail.com>
2021-01-01 22:56:01 +0000

    Removed absolute path to file, added executeQuery method

--- Server/src/main/java/com/webvowl/Server/SWRLAPI.java
+++ Server/src/main/java/com/webvowl/Server/SWRLAPI.java
@@ -13,35 +13,42 @@ import org.swrlapi.sqwrl.exceptions.SQWRLException;
 import java.io.File;
 
 public class SWRLAPI {
-    // Create OWLOntology instance using the OWLAPI
-    OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
-    OWLOntology ontology;
-
-    {
-        try {
-            ontology = ontologyManager.loadOntologyFromOntologyDocument(new File("C:\\Users\\Diogo Work\\Desktop\\Server\\owlUpload\\PMOEA.owl"));
-        } catch (OWLOntologyCreationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Create SQWRL query engine using the SWRLAPI
-    SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
-
-    // Create and execute a SQWRL query using the SWRLAPI
-    SQWRLResult result;
-    {
-        try {
-            result = queryEngine.runSQWRLQuery("q1","swrlb:add(?x, 2, 2) -> sqwrl:select(?x)");
-            if (result.next()) {
-                System.out.println("Name: " + result.getLiteral("x").getInteger());
+    
+    static Object executeQuery(String query, String owlFileName){
+        // Create OWLOntology instance using the OWLAPI
+        OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+        OWLOntology ontology = null;
+        {
+            try {
+                ontology = ontologyManager.loadOntologyFromOntologyDocument(new File("..\\Server\\owlUpload\\"+owlFileName+".owl"));
+            } catch (OWLOntologyCreationException e) {
+                e.printStackTrace();
             }
-        } catch (SWRLParseException e) {
-            e.printStackTrace();
-        } catch (SQWRLException e) {
-            e.printStackTrace();
         }
+
+        // Create SQWRL query engine using the SWRLAPI
+        SQWRLQueryEngine queryEngine = SWRLAPIFactory.createSQWRLQueryEngine(ontology);
+
+        // Create and execute a SQWRL query using the SWRLAPI
+        SQWRLResult result;
+        {
+            try {
+                result = queryEngine.runSQWRLQuery("q1",query);
+                System.out.println(result);
+                return result;
+
+                //if (result.next()) {
+                //    System.out.println("Name: " + result.getLiteral("x").getInteger());
+                //}
+            } catch (SWRLParseException e) {
+                e.printStackTrace();
+            } catch (SQWRLException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
     }
+    
 
     // Process the SQWRL result
 
